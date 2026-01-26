a953ec959f2b09a9e1fe140e688f83b909ad3768
Aryaswapnil07 <achintyasingh48@gmail.com>
2026-01-26 23:40:56 +0530

    first Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("../models/listing.js");
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+  res.send("HI I am root ");
+});
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing ({
+//         title : "My new Villa ",
+//         description : "By the Beach",
+//         price : 1200,
+//         location : "Calangute , Goa ",
+//         country : "India ",
+//     });
+//     await sampleListing.save();
+//     console.log("Sample was saved");
+//     res.send("succesfully testing ");
+// });
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080 ");
+});
+
